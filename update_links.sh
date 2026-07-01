a0f4fa0c31c49a026883b3be38c96c3116e688ba
pawanwashudev-official <216921330+pawanwashudev-official@users.noreply.github.com>
2026-07-01 02:51:23 +0000

    Update contact info and fix markdown rendering on product pages
    - Updated WhatsApp links across the site to use the user ID (@pawanwashudev)
    - Added Instagram contact links globally (Footer, About, CTA, Contact)
    - Migrated product JSONs to reflect actual GitHub repositories
    - Implemented `react-markdown` and `@tailwindcss/typography` to properly render markdown content in OrbitDetail.tsx

--- update_links.sh
+++
@@ -1,4 +0,0 @@
-sed -i 's/https:\/\/wa.me\/919279377276/https:\/\/wa.me\/pawanwashudev/g' src/components/Footer.tsx
-sed -i 's/https:\/\/wa.me\/919279377276/https:\/\/wa.me\/pawanwashudev/g' src/components/CTASection.tsx
-sed -i 's/https:\/\/wa.me\/919279377276/https:\/\/wa.me\/pawanwashudev/g' src/pages/About.tsx
-sed -i 's/https:\/\/wa.me\/919279377276/https:\/\/wa.me\/pawanwashudev/g' src/pages/Contact.tsx
